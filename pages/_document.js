80dfa7844b73c082d8875d61b8b0650ba6f38c40
brzhnkv <brzhnkv@gmail.com>
2021-02-08 19:38:14 +0500

    ++

--- pages/_document.js
+++ pages/_document.js
@@ -21,42 +21,42 @@ export default class MyDocument extends Document {
             href="/fonts/TRYVesterbro-Regular.woff2"
             as="font"
             type="font/woff2"
-            //  crossorigin="anonymous"
+            crossorigin="anonymous"
           />
           <link
             rel="preload"
             href="/fonts/TRYVesterbro-Medium.woff2"
             as="font"
             type="font/woff2"
-            //  crossorigin="anonymous"
+            crossorigin="anonymous"
           />
           <link
             rel="preload"
             href="/fonts/TRYVesterbro-Poster.woff2"
             as="font"
             type="font/woff2"
-            //  crossorigin="anonymous"
+            crossorigin="anonymous"
           />
           <link
             rel="preload"
             href="/fonts/TRYVesterbro-Light.woff2"
             as="font"
             type="font/woff2"
-            //  crossorigin="anonymous"
+            crossorigin="anonymous"
           />
           <link
             rel="preload"
             href="/fonts/TRYVesterbro-Bold.woff2"
             as="font"
             type="font/woff2"
-            //  crossorigin="anonymous"
+            crossorigin="anonymous"
           />
           <link
             rel="preload"
             href="/fonts/TRYVesterbro-Extrabold.woff2"
             as="font"
             type="font/woff2"
-            // crossorigin="anonymous"
+            crossorigin="anonymous"
           />
           <link rel="stylesheet" href="/fonts/fonts.css" />
           {/* Global Site Code Pixel - Facebook Pixel */}
